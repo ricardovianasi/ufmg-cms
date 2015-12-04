6d771d4c42a36c54c3e8d0c798df2bdc712f1b5a
Lucas Mezêncio <lucas.mezencio@gmail.com>
2015-12-04 11:04:50 -0200

    Change the way we work with envs

--- tasks/env.js
+++ tasks/env.js
@@ -1,19 +1,21 @@
-var gulp = require('gulp'),
-		gulpNgConfig = require('gulp-ng-config'),
-		fs = require('fs');
+var gulp = require('gulp');
+var gulpNgConfig = require('gulp-ng-config');
+var fs = require('fs');
 
+// Default environment is production
+var env = process.env.APP_ENV || 'production';
 
 gulp.task('env', function () {
-	var file;
+  var file = 'env-'+env+'.json';
 
-	fs.stat('app/env-local.json', function(err, stat) {
-	  if(err == null)
-	  	file = 'env-local.json'
-	  else 
-	  	file = 'env.json'
-	  
-	  gulp.src('./app/' + file)
-	  	.pipe(gulpNgConfig('env'))
-	  	.pipe(gulp.dest('./app/common/config/'))
-	});
+  fs.stat('app/env-local.json', function (err) {
+    // Default env file is local
+    if (err == null) {
+      file = 'env-local.json';
+    }
+
+    gulp.src('./app/'+file)
+      .pipe(gulpNgConfig('env'))
+      .pipe(gulp.dest('./app/common/config/'));
+  });
 });
