091a3f98570356cceceb7a70d7da21768d9001f0
Lucas Mezêncio <lucas.mezencio@gmail.com>
2015-12-04 10:28:00 -0200

    Change image directory

--- tasks/copyimage.js
+++ tasks/copyimage.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
 gulp.task('copyimage', function () {
-  return gulp.src('./app/assets/img/*.*')
+  return gulp.src('./app/assets/images/*.*')
     .pipe(gulp.dest('./build/assets/img'));
 });
