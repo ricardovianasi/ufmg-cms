dc39b0f9c91b98c2e62f55a2cc3c25193bbda3a9
Lucas Mezêncio <lucas.mezencio@gmail.com>
2015-12-15 14:47:33 -0200

    Add option to save as draft on publishment box

--- app/components/publishment/publishment.directive.js
+++ app/components/publishment/publishment.directive.js
@@ -1,32 +1,44 @@
-;(function(){
-'use strict';
-angular
-  .module('componentsModule')
-  .directive('publishmentOptions', [
-    'StatusService', function (StatusService) {
+;(function () {
+  'use strict';
 
-      return {
-        restrict: 'E',
-        templateUrl: 'components/publishment/publishment.template.html',
-        scope: {
-          obj: '=routeModel'
-        },
-        link: function ($scope, element, attrs) {
-          console.log('... PublishmentDirective');
+  angular.module('componentsModule')
+    .directive('publishmentOptions', [
+      '$timeout',
+      'StatusService',
+      function ($timeout, StatusService) {
 
-          var obj = attrs.routeModel;
+        return {
+          restrict: 'E',
+          templateUrl: 'components/publishment/publishment.template.html',
+          scope: {
+            obj: '=routeModel'
+          },
+          link: function ($scope, element, attrs) {
+            console.log('... PublishmentDirective');
 
-          $scope.obj = $scope.$parent[obj];
-          $scope.publish = $scope.$parent.publish;
-          $scope.remove = $scope.$parent.remove;
-          $scope.statuses = {};
+            var obj = attrs.routeModel;
 
-          // Statuses
-          StatusService.getStatus().then(function (data) {
-            $scope.statuses = data.data;
-          });
-        }
-      };
-    }
-  ]);
+            $scope.obj = $scope.$parent[obj];
+            $scope.publish = $scope.$parent.publish;
+            $scope.remove = $scope.$parent.remove;
+            $scope.statuses = [];
+
+            $scope.saveDraft = function ($event) {
+              $event.stopPropagation();
+
+              $scope.obj.status = StatusService.STATUS_DRAFT;
+
+              $timeout(function () {
+                angular.element('#publish-button').trigger('click');
+              }, 100);
+            };
+
+            // Statuses
+            StatusService.getStatus().then(function (data) {
+              $scope.statuses = data.data;
+            });
+          }
+        };
+      }
+    ]);
 })();
