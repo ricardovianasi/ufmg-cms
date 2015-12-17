2cdd0ce520205454cf5467a0e9d0a254fc04cb9a
dielduarte <magdiel.kesller@gmail.com>
2015-12-17 15:35:22 -0200

    remove gulp-jade

--- tasks/watch.js
+++ tasks/watch.js
@@ -3,5 +3,5 @@ var gulp = require('gulp');
 gulp.task('watch', function () {
   gulp.watch('app/**/*.js', ['test-js', 'refresh']);
   gulp.watch('app/assets/scss/**/*.scss', ['css', 'refresh']);
-  gulp.watch('app/**/*.jade', ['jade', 'refresh']);
+  gulp.watch('app/**/*.html', ['html', 'refresh']);
 });
