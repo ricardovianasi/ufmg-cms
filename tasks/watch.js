aa0469d3d11eb064ffd6a2bcb3b2d30accfc3f38
Tiago Porto <tiago@tiagoporto.com>
2015-12-18 16:51:41 -0200

    Page new midia

--- tasks/watch.js
+++ tasks/watch.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 
 gulp.task('watch', function () {
   gulp.watch('app/**/*.js', ['test-js', 'refresh']);
-  gulp.watch('app/assets/scss/**/*.scss', ['css', 'refresh']);
+  gulp.watch('app/assets/styles/**/*.{scss,sass}', ['css', 'refresh']);
   gulp.watch('app/**/*.html', ['html', 'refresh']);
 });
