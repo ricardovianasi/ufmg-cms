77e52cd67832abf2d51b7c3ed63d23c3b4acaae7
Lucas Mezêncio <lucas.mezencio@gmail.com>
2015-12-22 09:58:45 -0200

    Reindent lines and change public function names

--- app/components/modules/UploadComponentController.controller.js
+++ app/components/modules/UploadComponentController.controller.js
@@ -1,95 +1,89 @@
-(function(){
+(function () {
   'use strict';
 
-  angular
-    .module('componentsModule')
+  angular.module('componentsModule')
     .controller('UploadComponentController', UploadComponentController);
 
-    UploadComponentController.$inject = ['dataTableConfigService',
-                                         '$scope',
-                                         'MediaService'];
+  UploadComponentController.$inject = [
+    'dataTableConfigService',
+    '$scope',
+    'MediaService'
+  ];
 
-    function UploadComponentController(dataTableConfigService, $scope, MediaService){
-      var vm = this;
+  function UploadComponentController(dataTableConfigService, $scope, MediaService) {
+    var vm = this;
 
-      vm.tabs = {
-        home: false,
-        midia: false
-      };
+    vm.tabs = {
+      home: false,
+      midia: false
+    };
 
-      vm.currentFile = {
-        url: '',
-        id: '',
-        author: '',
-        legend: ''
-      };
+    vm.currentFile = {
+      url: '',
+      id: '',
+      author: '',
+      legend: ''
+    };
 
-      vm._openMidia = _openMidia;
-      vm._changePage = _changePage;
-      vm._selectMidia = _selectMidia;
+    vm.openMidia = _openMidia;
+    vm.changePage = _changePage;
+    vm.selectMidia = _selectMidia;
 
-
-      /**
-      *  watch for vm.add_photos model
-      *
-      */
-      $scope.$watch('vm.add_photos', function () {
-        if (vm.add_photos) {
-          MediaService.newFile(vm.add_photos).then(function (data) {
-            vm.currentFile = {
-              url: data.url,
-              id: data.id,
-              author: data.author.name,
-              legend: data.legend
-            };
-          });
-        }
-      });
-
-
-      /**
-      *  _openMidia Function
-      * open tab media, and call _loadMidia function
-      */
-
-      function _openMidia(){
-        vm.tabs.midia = true;
-        _loadMidia();
-      }
-
-      /**
-      *  _loadMidia Function
-      * get all media
-      */
-
-      function _loadMidia(page){
-        MediaService.getMedia(page, 27).then(function(result){
-          vm.midia = result.data;
-          console.log(vm.midia);
+    /**
+     *  watch for vm.add_photos model
+     *
+     */
+    $scope.$watch('vm.add_photos', function () {
+      if (vm.add_photos) {
+        MediaService.newFile(vm.add_photos).then(function (data) {
+          vm.currentFile = {
+            url: data.url,
+            id: data.id,
+            author: data.author.name,
+            legend: data.legend
+          };
         });
       }
+    });
 
-      /**
-      *  _changePage Function
-      *
-      */
-
-      function _changePage(){
-        _loadMidia(vm.currentPage);
-      }
-
-      /**
-      *  _selectMidia Function
-      *
-      */
-
-      function _selectMidia(id, author, legend, url){
-        vm.currentFile = {
-          url: url,
-          id: id,
-          author: author,
-          legend: legend
-        };
-      }
+    /**
+     *  _openMidia Function
+     * open tab media, and call _loadMidia function
+     */
+    function _openMidia() {
+      vm.tabs.midia = true;
+      _loadMidia();
     }
+
+    /**
+     *  _loadMidia Function
+     * get all media
+     */
+    function _loadMidia(page) {
+      MediaService.getMedia(page, 27).then(function (result) {
+        vm.midia = result.data;
+      });
+    }
+
+    /**
+     *  _changePage Function
+     *
+     */
+    function _changePage() {
+      _loadMidia(vm.currentPage);
+    }
+
+    /**
+     *  _selectMidia Function
+     *
+     */
+    function _selectMidia(id, author, legend, url) {
+      vm.currentFile = {
+        url: url,
+        id: id,
+        author: author,
+        legend: legend
+      };
+    }
+  }
 })();
