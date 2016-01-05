93b77b117acb045cd3f5d0f559efd57dfc116eb6
dielduarte <magdiel.kesller@gmail.com>
2016-01-05 10:50:02 -0200

    edit redactor config

--- app/common/config/app.config.js
+++ app/common/config/app.config.js
@@ -16,33 +16,16 @@
         $rootScope.redactorConfig = {
           lang: 'pt_br',
           plugins: ['imagencrop'],
-          linebreaks: false,
-          formatting: ['p', 'blockquote', 'pre', 'h1', 'h2', 'h3', 'h4', 'h5'],
           buttons: [
             'html',
-            'formatting',
+            'format',
             'bold',
             'italic',
             'link',
-            //'deleted',
-            //'unorderedlist',
-            //'orderedlist',
-            //'outdent',
-            //'indent',
             'imagencrop',
-            //'image',
             'file',
-            'adasda'
-            //'alignment',
-            //'horizontalrule',
           ],
-          allowedAttr: [
-            ['section', 'class'],
-            ['div', 'class'],
-            ['img', ['src', 'alt', 'title']],
-            ['figure', 'class'],
-            ['a', ['href', 'title']]
-          ]
+          tabKey: false,
         };
       }
     ]);
