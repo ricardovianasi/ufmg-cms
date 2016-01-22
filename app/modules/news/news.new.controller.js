4c85eaf058b5e683fa7adeefa8df2f0818a01d78
Lucas Mezêncio <lucas.mezencio@gmail.com>
2016-01-22 16:29:04 -0200

    Reindent lines

--- app/modules/news/news.new.controller.js
+++ app/modules/news/news.new.controller.js
@@ -69,9 +69,9 @@
     });
 
     $scope.publish = function (data, preview) {
-
-      if(typeof data.highlight_ufmg == 'undefined')
+      if (typeof data.highlight_ufmg == 'undefined') {
         data.highlight_ufmg = false;
+      }
 
       var _obj = {
         title: data.title,
