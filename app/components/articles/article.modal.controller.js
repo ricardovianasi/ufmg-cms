2b8095cf7b966fd9246147a8f6cfd2359d996398
Lucas Mezêncio <lucas.mezencio@gmail.com>
2016-01-25 08:42:35 -0200

    Fix layout according to prototype

--- app/components/articles/article.modal.controller.js
+++ app/components/articles/article.modal.controller.js
@@ -7,12 +7,23 @@
   ArticleModalController.$inject = [
     '$scope',
     '$uibModalInstance',
+    '$uibModal',
+    '$timeout',
     'article',
     'MediaService',
-    '$timeout'
   ];
 
-  function ArticleModalController($scope, $uibModalInstance, article, MediaService, $timeout) {
+  /**
+   * @param $scope
+   * @param $uibModalInstance
+   * @param $uibModal
+   * @param $timeout
+   * @param article
+   * @param MediaService
+   *
+   * @constructor
+   */
+  function ArticleModalController($scope, $uibModalInstance, $uibModal, $timeout, article, MediaService) {
     console.log('... ArticleModalController');
 
     if (article) {
@@ -92,31 +103,48 @@
 
     // Upload
     // Cover Image - Upload
-    $scope.$watch('article_cover', function () {
-      if ($scope.article_cover) {
-        $scope.upload([$scope.article_cover], 'cover');
-      }
-    });
-
     $scope.$watch('article_thumb', function () {
       if ($scope.article_thumb) {
-        $scope.upload([$scope.article_thumb], 'thumb');
+        $scope.upload($scope.article_thumb);
       }
     });
 
-    $scope.upload = function (files, type) {
-      angular.forEach(files, function (file) {
-        MediaService.newFile(file).then(function (data) {
-          $scope.article[type] = data.id;
-          $scope.article[type+'_url'] = data.url;
-        });
+    /**
+     * @param file
+     */
+    $scope.upload = function (file) {
+      MediaService.newFile(file).then(function (data) {
+        $scope.article.thumb = data.id;
+        $scope.article.thumb_url = data.url;
+      });
+    };
+
+    /**
+     *
+     */
+    $scope.uploadImage = function () {
+      var moduleModal = $uibModal.open({
+        templateUrl: 'components/modal/upload-component.template.html',
+        controller: 'UploadComponentController as vm',
+        backdrop: 'static',
+        size: 'xl',
+        resolve: {
+          formats: function () {
+            return ['pageCover'];
+          }
+        }
+      });
+
+      moduleModal.result.then(function (data) {
+        $scope.article.cover = data.id;
+        $scope.article.cover_url = data.url;
       });
     };
 
     $scope.removeImage = function (type) {
       $timeout(function () {
         $scope.article[type] = '';
-        $scope.article[type+'_url'] = '';
+        $scope.article[type + '_url'] = '';
 
         $scope.$apply();
       });
