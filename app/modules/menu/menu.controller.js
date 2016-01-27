1c80a6b42fe47d49232412e4ed885728fdff9aa2
Lucas Mezêncio <lucas.mezencio@gmail.com>
2016-01-27 17:17:48 -0200

    Add sortable list for menu

--- app/modules/menu/menu.controller.js
+++ app/modules/menu/menu.controller.js
@@ -30,11 +30,84 @@
 
     var vm = this;
 
+    var pages = [];
+    vm.pages = [];
     vm.menu = [];
-    vm.pages = {};
+
+    vm.sortableOptions = {
+      placeholder: 'list-group-item'/*{
+        element: function (currentItem) {
+          var item = angular.copy(currentItem);
+          var $item = jQuery(item);
+
+          $item.addClass('placeholder');
+
+          return $item[0];
+        },
+        update: function (container, p) {
+          return;
+        }
+      }*/,
+      connectWith: '.main',
+      /**
+       * @param e
+       * @param ui
+       */
+      stop: function (e, ui) {
+        // if the element is removed from the first container
+        if (
+          $(e.target).hasClass('selectable') &&
+          ui.item.sortable.droptarget &&
+          e.target != ui.item.sortable.droptarget[0]
+        ) {
+          // clone the original model to restore the removed item
+          vm.pages = pages.slice();
+        }
+      },
+      /**
+       * @param event
+       * @param ui
+       */
+      update: function(event, ui) {
+        // on cross list sortings recieved is not true
+        // during the first update
+        // which is fired on the source sortable
+        if (!ui.item.sortable.received) {
+          var originNgModel = ui.item.sortable.sourceModel;
+          var itemModel = originNgModel[ui.item.sortable.index];
+
+          // check that its an actual moving
+          // between the two lists
+          if (
+            originNgModel == vm.pages &&
+            ui.item.sortable.droptargetModel == vm.menu
+          ) {
+            var exists = !!vm.menu.filter(function(x) {
+              return x.title === itemModel.title;
+            }).length;
+
+            if (exists) {
+              ui.item.sortable.cancel();
+            }
+          }
+        }
+      },
+    };
+
+    vm.removeItem = _removeItem;
+
+    /**
+     * @param idx
+     *
+     * @private
+     */
+    function _removeItem(idx) {
+      vm.menu.splice(idx, 1);
+    }
 
     PagesService.getPages().then(function (data) {
-      vm.pages = data.data;
+      pages = data.data.items;
+      vm.pages = pages.slice();
     });
   }
 })();
