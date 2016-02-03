34de178fc4a77f6ce25804cd9d7f60c4980337b7
Lucas Mezêncio <lucas.mezencio@gmail.com>
2016-02-03 13:47:54 -0200

    Move featured

--- app/modules/periodical/featured/featured.route.js
+++ app/modules/periodical/featured/featured.route.js
@@ -7,17 +7,17 @@
 
       $routeProvider
         .when('/featured', {
-          templateUrl: 'modules/featured/featured.template.html',
+          templateUrl: 'modules/periodical/featured/featured.template.html',
           controller: 'featuredController',
           controllerAs: 'vm'
         })
         .when('/featured/new', {
-          templateUrl: 'modules/featured/featured.form.template.html',
+          templateUrl: 'modules/periodical/featured/featured.form.template.html',
           controller: 'featuredNewController',
           controllerAs: 'vm'
         })
         .when('/featured/edit/:id', {
-          templateUrl: 'modules/featured/featured.form.template.html',
+          templateUrl: 'modules/periodical/featured/featured.form.template.html',
           controller: 'featuredEditController',
           controllerAs: 'vm'
         });
