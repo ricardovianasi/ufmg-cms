c6b40d36937c6424f01a73a3301df946cfe1f45b
Henrique Rodrigues Moreira <henryqrm@gmail.com>
2017-01-16 13:35:25 -0200

    Mudança para ngInject

--- app/services/notification.service.js
+++ app/services/notification.service.js
@@ -1,28 +1,26 @@
-;(function () {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular.module('serviceModule')
-    .factory('NotificationService', NotificationService);
+    angular.module('serviceModule')
+        .factory('NotificationService', NotificationService);
 
-  NotificationService.$inject = ['toastr'];
+    /** @ngInject */
+    function NotificationService(toastr, $log) {
+        $log.info('NotificationService');
 
-  function NotificationService(toastr) {
-    console.log('... NotificationService');
-
-    return {
-      success: function (title, msg) {
-        toastr.success(title, msg);
-
-      },
-      error: function (title, msg) {
-        toastr.error(title, msg);
-      },
-      warning: function (title, msg) {
-        toastr.warning(title, msg);
-      },
-      info: function (title, msg) {
-        toastr.info(title, msg);
-      }
-    };
-  }
+        return {
+            success: function (title, msg) {
+                toastr.success(title, msg);
+            },
+            error: function (title, msg) {
+                toastr.error(title, msg);
+            },
+            warning: function (title, msg) {
+                toastr.warning(title, msg);
+            },
+            info: function (title, msg) {
+                toastr.info(title, msg);
+            }
+        };
+    }
 })();
