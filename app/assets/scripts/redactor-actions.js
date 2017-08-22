4c8d35d45cff67829ec3b95550fb9d0bde897e84
Henrique Rodrigues Moreira <henryqrm@gmail.com>
2017-08-22 14:29:23 -0300

    redactor actions: remover

---
+++ app/assets/scripts/redactor-actions.js
@@ -0,0 +1,21 @@
+onInit();
+
+function onInit() {
+    widgetButtonRemove();
+}
+
+function widgetButtonRemove() {
+    $(document).on('mouseenter', '.figure-removable', function () {
+        $(this).append(
+            '<span class="remove-button btn btn-danger btn-widget-remove"><i class="fa fa-trash"></i></span>'
+        );
+    });
+
+    $(document).on('mouseleave', '.figure-removable', function () {
+        $('.remove-button').remove();
+    });
+
+    $(document).on('click', '.remove-button', function (event) {
+        $(this).closest('.figure-removable').remove();
+    });
+}
