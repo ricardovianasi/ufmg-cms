51fe88ba66c3c03ac85005a5a7de9fe6af1dd1ae
Henrique Rodrigues Moreira <henryqrm@gmail.com>
2017-09-18 15:29:04 -0300

    fix router duplicate

--- app/modules/periodical/periodical.route.js
+++ app/modules/periodical/periodical.route.js
@@ -20,7 +20,7 @@
                     templateUrl: 'modules/periodical/editions/periodical-editions.template.html',
                     controller: 'PeriodicalEditionsController'
                 })
-                .when('/periodical/:id/editions/edit/:id', {
+                .when('/periodical/:id/editions/edit/:edition', {
                     templateUrl: 'modules/periodical/editions/periodical-editions.edition.form.template.html',
                     controller: 'PeriodicalEditionEditController'
                 })
