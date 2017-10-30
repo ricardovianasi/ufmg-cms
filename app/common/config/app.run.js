78b209c00bc9ac829836ced65bcb00e12c20e311
Henrique Rodrigues <email.henrique.rm@gmail.com>
2017-10-30 15:32:05 -0200

    Sidebar com toggle

--- app/common/config/app.run.js
+++ app/common/config/app.run.js
@@ -306,6 +306,9 @@
         PermissionService
     ) {
         $rootScope.shownavbar = true;
+
+        $rootScope.isMobile = $window.screen.availWidth > 768;
+        $rootScope.isActiveSidebar = angular.copy($rootScope.isMobile);
         $rootScope.viewOnly = false;
         $rootScope.moduleCurrent = false;
 
@@ -313,6 +316,10 @@
         $rootScope.canPut = PermissionService.canPut;
         $rootScope.canDelete = PermissionService.canDelete;
         $rootScope.canGet = PermissionService.canGet;
+
+        $rootScope.sidebarToggle = function () {
+            $rootScope.isActiveSidebar = !$rootScope.isActiveSidebar;
+        };
 
         $rootScope.ngScrollbarsConfig = {
             autoHideScrollbar: true,
