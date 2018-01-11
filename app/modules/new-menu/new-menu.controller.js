4d76b37250d0de15a4bfe4bae97b1bdf7fdf270e
Lucas Carvalho <lucasccorrea@gmail.com>
2018-01-11 15:00:36 -0200

    include mock data in controller, html dynamically, partial implements ui sortable

--- app/modules/new-menu/new-menu.controller.js
+++ app/modules/new-menu/new-menu.controller.js
@@ -23,6 +23,113 @@
 
         ////////////////
 
-        function activate() { }
+        function initConfigSortable() {
+            vm.sortableOptions = {
+                connectWith: '.list'
+            }
+        }
+
+        function initMock() {
+            vm.items = [
+                {
+                    id: 10,
+                    label: 'A Universidade',
+                    children: [
+                        {
+                            id: 11,
+                            label: 'Apresentação',
+                            children: [
+                                {
+                                    id: 111,
+                                    label: 'UFMG e a Cidade',
+                                    children: []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    id: 20,
+                    label: 'Cursos',
+                    children: [
+                        {
+                            id: 21,
+                            label: 'Graduação',
+                            children: [ ]
+                        },
+                        {
+                            id: 22,
+                            label: 'Pós Graduação',
+                            children: [
+                                {
+                                    id: 221,
+                                    label: 'Especialização',
+                                    children: []
+                                },
+                                {
+                                    id: 222,
+                                    label: 'Mestrado',
+                                    children: []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    id: 30,
+                    label: 'Pesquisa e Inovação',
+                    children: [
+                        {
+                            id: 31,
+                            label: 'Extensão',
+                            children: [
+                                {
+                                    id: 311,
+                                    label: 'Especialização e Produção Científica',
+                                    children: []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    id: 40,
+                    label: 'Formas de Ingresso',
+                    children: [
+                        {
+                            id: 41,
+                            label: 'Ingresso em Graduação',
+                            children: []
+                        },
+                        {
+                            id: 42,
+                            label: 'Ingresso em Pós Graduação',
+                            children: []
+                        },
+                        {
+                            id: 43,
+                            label: 'Ingresso no Ensino Médio e Técnico',
+                            children: []
+                        },
+                        {
+                            id: 44,
+                            label: 'Ingresso no Ensino Fundamental',
+                            children: []
+                        },
+                        {
+                            id: 45,
+                            label: 'Ingresso na Educação de Jovens e Adultos',
+                            children: []
+                        }
+                    ]
+                }
+            ];
+            console.log(vm.items);
+        }
+
+        function activate() {
+            initMock();
+            initConfigSortable();
+        }
     }
 })();
