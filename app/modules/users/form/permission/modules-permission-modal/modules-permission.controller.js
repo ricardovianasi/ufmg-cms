890d2ad034f9945bcb1380e6362063adac53ad09
Lucas Carvalho <lucasccorrea@gmail.com>
2018-02-28 17:02:24 -0300

    init modal of permission module, select page

--- app/modules/users/form/permission/modules-permission-modal/modules-permission.controller.js
+++ app/modules/users/form/permission/modules-permission-modal/modules-permission.controller.js
@@ -6,14 +6,61 @@
         .controller('ModulesPermissionController', ModulesPermissionController);
 
     /** ngInject */    
-    function ModulesPermissionController(WidgetsService) {
+    function ModulesPermissionController(WidgetsService, PagesService, Util) {
         let vm = this;
+
+        vm.loadMorePage = loadMorePage;
         
-
         activate();
-
+        
         ////////////////
 
-        function activate() { }
+        function loadMorePage(search) {
+            console.log('loadMorePage', search, vm.currentSearch, vm.countPage);
+            if(search !== vm.currentSearch) {
+                _initParamsSelectPage();
+                vm.currentSearch = search;
+            }
+            vm.countPage += 1;
+            let paramsHandled = _getParamsPages(vm.currentSearch, vm.countPage);
+            _getPages(paramsHandled); 
+
+        }
+
+        function _getPages(params) {
+            PagesService.getPages(params)
+                .then(function(data) {
+                    vm.pages = vm.pages.concat(data.data.items);
+                    console.log('_getPages then', data.data.items, vm.pages);
+                });
+        }
+
+        function _getParamsPages(search, countPage) {
+            let params = {
+                page: countPage,
+                page_size: 10,
+                search: search,
+                order_by: {
+                    field: 'title',
+                    direction: 'ASC'
+                }
+            }
+            return Util.getParams(params, 'title');
+        }
+
+        function _initParamsSelectPage() {
+            vm.countPage = 0;
+            vm.pages = [];
+            vm.currentSearch = '';
+        }
+
+        function _initVariables() {
+            vm.pageSelected;
+            _initParamsSelectPage();
+        }
+
+        function activate() {
+            _initVariables();
+        }
     }
 })();
