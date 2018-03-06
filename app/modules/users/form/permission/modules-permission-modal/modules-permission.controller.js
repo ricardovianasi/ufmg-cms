6629ce32099828bd62df0d7d08b6301fdfd6be9f
Lucas Carvalho <lucasccorrea@gmail.com>
2018-03-06 16:57:02 -0300

    inclusao do select modules to modal permission

--- app/modules/users/form/permission/modules-permission-modal/modules-permission.controller.js
+++ app/modules/users/form/permission/modules-permission-modal/modules-permission.controller.js
@@ -9,16 +9,32 @@
     function ModulesPermissionController(WidgetsService, PagesService, Util, dataPermissionModule) {
         let vm = this;
 
+        vm.onWidgetSelected = onWidgetSelected;
+
         activate();
         
         ////////////////
 
+        function onWidgetSelected(item) {
+            console.log(item);
+        }
+
         function _initVariables() {
             vm.pageSelected;
+            vm.widgetSelected;
+        }
+
+        function _loadWidgets() {
+            WidgetsService.getWidgets()
+                .then(function(data) {
+                    vm.widgets = data.data.items;
+                    console.log(data);
+                });
         }
 
         function activate() {
             _initVariables();
+            _loadWidgets();
         }
     }
 })();
