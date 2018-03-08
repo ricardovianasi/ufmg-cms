4dd43f2c9128ff4f831bb36209e0bb4670dd688c
Lucas Carvalho <lucasccorrea@gmail.com>
2018-03-08 11:42:32 -0300

    add feature of pagination on component table-full-data

--- app/components/table-full-data-component/table-full-data.component.js
+++ app/components/table-full-data-component/table-full-data.component.js
@@ -24,17 +24,44 @@
         let ctrlTable = this;
 
         ctrlTable.getTotCols = getTotCols;
+        ctrlTable.changeTable = changeTable;
+        ctrlTable.showPagination = showPagination;
 
         ////////////////
 
-        ctrlTable.$onInit = function() { };
         ctrlTable.$onChanges = function(changesObj) { };
         ctrlTable.$onDestroy = function() { };
+
+        function showPagination() {
+            return (ctrlTable.fullDatasFiltered.length / ctrlTable.limit) > 1; 
+        }
+
+        function changeTable() {
+            setBoundResults();
+        }
+
+        function setBoundResults() {
+            let startBound = ctrlTable.limit * (ctrlTable.configPagination.page - 1);
+            ctrlTable.boundResults = {
+                start: startBound + 1,
+                end: startBound + ctrlTable.limit
+            }
+        }
 
         function getTotCols() {
             let lengthCols = ctrlTable.config.cols ? ctrlTable.config.cols.length : 0;
             let lengthColsActions = ctrlTable.config.actions ? ctrlTable.config.actions.length : 0;
             return lengthCols + lengthColsActions;
         }
+
+        function _initVariables() {
+            ctrlTable.configPagination = { page: 1 };
+            ctrlTable.fullDatasFiltered = [];
+            setBoundResults();
+        }
+
+        ctrlTable.$onInit = function() {
+            _initVariables();
+        };
     }
 })();
