7a94b80bff708bb96240f79f57bd0b38e04475bc
Lucas Carvalho <lucasccorrea@gmail.com>
2018-03-15 11:40:16 -0300

    add conditions to add page on permissions

--- app/modules/users/form/permission/custom-permission-page/custom-permission-page.controller.js
+++ app/modules/users/form/permission/custom-permission-page/custom-permission-page.controller.js
@@ -6,7 +6,7 @@
         .controller('CustomPermissionPageController', CustomPermissionPageController);
 
     /** ngInject */
-    function CustomPermissionPageController($q, data, PagesService, WidgetsService) {
+    function CustomPermissionPageController($q, data, PagesService, WidgetsService, NotificationService) {
         let vm = this;
 
         vm.deletePage = deletePage;
@@ -24,7 +24,7 @@
         }
 
         function addPage() {
-            if(!vm.pageToAdd) {
+            if(!_canAddedPage(vm.pageToAdd)) {
                 return;
             }
             vm.dataList.unshift(_createPageToAdd(vm.pageToAdd));
@@ -35,9 +35,9 @@
         }
 
         function deletePage(result, type, idx) {
-            if(!result) {
-                endDialodDelete(type, idx);
-                return;
+            endDialodDelete(type, idx);
+            if(result) {
+                vm.dataList.splice(idx, 1);
             }
         }
 
@@ -48,6 +48,27 @@
         function startDialogDelete(type, idx) {
             vm.toggleDelete[type+idx] = true;
             console.log('startDialogDelete', vm.toggleDelete);
+        }
+
+        function _canAddedPage(page) {
+            let result = false;
+            let isAdded = _checkPageAdded(page);
+            if(page && !isAdded) {
+                result = true;
+            } else if(!page) {
+                NotificationService.warn('Atenção, deve ser selecionada uma página antes de inserir.');
+            } else if(isAdded) {
+                NotificationService.warn('Atenção, esta página já foi inserida.');
+            }
+            return result;
+        }
+
+        function _checkPageAdded(page) {
+            if(!page) {return false; };
+            let idx = vm.dataList.findIndex(function(pgAdded) {
+                return pgAdded.idPage === page.id
+            });
+            return idx >= 0;
         }
 
         function _createPageToAdd(page) {
