2a8411cd1c6eea7e6fa0d7414b96be9f9e03743f
Ricardo Viana <ricardovianasi@gmail.com>
2018-04-04 15:22:19 -0300

    novo widget

--- app/components/modal/widget-partials/sidebarbuttonradio/sidebarbuttonradio.service.js
+++ app/components/modal/widget-partials/sidebarbuttonradio/sidebarbuttonradio.service.js
@@ -7,6 +7,25 @@
 
     /** ngInject */
     function SidebarButtonRadioService() {
-        return {};
+        var service = {
+            load: load,
+            parseToLoad: parseToLoad,
+            parseToSave: parseToSave
+        };
+
+        return service;
+
+        ////////////////
+        function load(ctrl) {
+        }
+
+        function parseToLoad(widget) {
+            return widget;
+        }
+
+        function parseToSave(widget) {
+            return widget;
+        }
+
     }
 })();
