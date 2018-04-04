e600a58ee79d8e9157ecf96a2effd8594c85a2eb
Ricardo Viana <ricardovianasi@gmail.com>
2018-04-04 15:20:36 -0300

    novo widget

---
+++ app/components/modal/widget-partials/sidebarbuttonradio/sidebarbuttonradio.service.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('serviceModule')
+        .factory('SidebarButtonRadioService', SidebarButtonRadioService);
+
+    /** ngInject */
+    function SidebarButtonRadioService() {
+        return {};
+    }
+})();
