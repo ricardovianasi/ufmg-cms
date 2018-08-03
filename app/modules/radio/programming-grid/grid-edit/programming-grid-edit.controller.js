7f29330e2a235a64cefe323580168efb04f16614
Lucas Carvalho <lucasccorrea@gmail.com>
2018-08-03 16:38:43 -0300

    save program grid

--- app/modules/radio/programming-grid/grid-edit/programming-grid-edit.controller.js
+++ app/modules/radio/programming-grid/grid-edit/programming-grid-edit.controller.js
@@ -6,11 +6,14 @@
         .controller('GridEditController', GridEditController);
 
     /** ngInject */
-    function GridEditController(RadioService) {
+    function GridEditController(RadioService, $q, toastr) {
         var vm = this;
 
         vm.changeDay = changeDay;
+        vm.addProgram = addProgram;
+
         vm.listPrograms = [];
+        vm.loading = false;
         vm.weekDayActive;
 
         activate();
@@ -22,7 +25,40 @@
                 day.active = false;
             });
             weekDay.active = true;
-            vm.weekDayActive = weekDay.code;
+            vm.weekDayActive = weekDay;
+        }
+
+        function addProgram(program, parent, time) {
+            console.log('addProgram', vm.weekDayActive, program, parent, vm.time);
+            vm.loading =  true;
+            var progressPromise = parent ? _updateProgramParent(program, parent) : $q.resolve();
+            progressPromise
+                .then(function() {
+                    return _saveGrid(program, time);
+                })
+                .then(function(res) {
+                    vm.listPrograms.unshift(res.data);
+                    toastr.success('Programa inserido com sucesso.');
+                })
+                .catch(function(error) {console.error(error);})
+                .finally(function() { vm.loading = false; });
+        }
+
+        function _updateProgramParent(child, parent) {
+            child.id_parent = parent.id;
+            delete child.author;
+            delete child.grid_program;
+            return RadioService.updateProgram(child, child.id);
+        }
+
+        function _saveGrid(program, time) {
+            var programGrid = {
+                programming: program.id,
+                week_day: vm.weekDayActive.code,
+                time_start: moment(time.time_start).format('HH:mm'),
+                time_end: moment(time.time_end).format('HH:mm'),
+            };
+            return RadioService.registerProgramGrid(programGrid);
         }
 
         function _loadGrid() {
@@ -43,7 +79,7 @@
                 {label: 'Sexta-Feira', active: false, code: 5},
                 {label: 'Sábado', active: false, code: 6},
             ];
-            vm.weekDayActive = 7;
+            vm.weekDayActive = vm.weekdays[0];
             _loadGrid();
         }
     }
